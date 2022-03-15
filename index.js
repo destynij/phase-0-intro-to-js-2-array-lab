817af22d690ca8ed21d3fb867e66ec5aa954b7c9
Destyni <Destyninjohnson20@icloud.com>
2022-03-15 18:04:31 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+console.log(cats);
+beforeEach(function() {
+    cats.length = 0;
+    cats.push("Milo", "Otis", "Garfield");
+});
+function destructivelyAppendCat(name){
+    return name = cats.push("Ralph")
+}
+function destructivelyPrependCat(name){
+    return name = cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(name){
+    return name = cats.pop("Ralph")
+}
+function destructivelyRemoveFirstCat(name){
+    return name = cats.shift("Bob")
+}
+
+beforeEach(function() {
+    cats.length = 0;
+    cats.push("Milo","Otis","Garfield");
+});
+function appendCat(name) {
+    return name = [...cats, "Broom"]
+}
+
+function prependCat(name) {
+    return name = ["Arnold", ...cats]
+}
+
+
+beforeEach(function() {
+    cats.length = 0;
+    cats.push("Milo", "Otis", "Garfield")
+});
+function removeLastCat(name) {
+    return name = cats.slice(0, 2)
+}
+
+beforeEach(function() {
+    cats.length = 0;
+    cats.push("Milo", "Otis", "Garfield");
+});
+
+function removeFirstCat(name) {
+    return name = cats.slice(1)
+}
